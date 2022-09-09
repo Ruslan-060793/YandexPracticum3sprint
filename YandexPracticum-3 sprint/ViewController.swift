c9fb04b4d2ecdff615a65fe4eb6ac6c79dd10d0f
Ruslan-060793 <rp@iclicks.io>
2022-09-09 12:26:23 +0300

    Добавлена логика инкремента значения при нажатию по кнопке

--- YandexPracticum-3 sprint/ViewController.swift
+++ YandexPracticum-3 sprint/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var value: Int = 0
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        valueLabel.text = "Значение счётчика: \(value)"
     }
 
-
+    @IBAction func incrementValueBtn(_ sender: UIButton) {
+        value += 1
+        valueLabel.text = "Значение счётчика: \(value)"
+    }
+    
 }
 
